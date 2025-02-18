da1aab505d4cb0de7cd74dd47fd44ea197cf022c
Moritz Brandstätter <arriggon@gmail.com>
2025-02-18 15:22:45 +0100

    update build process

--- build.sh
+++ build.sh
@@ -2,16 +2,20 @@
 
 # remove build if exists
 if [ -d "build" ]; then
+    echo "Removing old build directory"
     rm -rf "build"
 fi
 # creating build-directory
+echo "Creating build directory"
 mkdir "build"
 
 # copy src-contents
+echo "Copy files to build directory"
 cp -R src/. build/
 
 # copy styles
 cp -R styles build/
 
 # remove input.css file
+echo "Remove unused input.css"
 rm build/styles/input.css
